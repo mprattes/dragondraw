5e7464252b17aafc9cef2fa053298f509b6c9f11
mprattes <52427969+mprattes@users.noreply.github.com>
2021-05-17 18:40:15 -0300

    Add files via upload

---
+++ Assets/Scripts/Fase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Fase : MonoBehaviour
+{
+   
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+        }
+    }
+    
+    
+}
